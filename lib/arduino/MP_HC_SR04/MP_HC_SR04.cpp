b4e6735d596c2ac6932692b8fa84ba072e00d64a
Chattriya Jariyavajee <chattriya.j@mail.kmutt.ac.th>
2018-09-22 14:12:08 +0700

    Add MP_HC_SR04

--- lib/arduino/MP_HC_SR04/MP_HC_SR04.cpp
+++ lib/arduino/MP_HC_SR04/MP_HC_SR04.cpp
@@ -1,29 +1,44 @@
-#include "MP_HC_SR04.h"
+#include "MP_HY_SR04.h"
 
-MP_HC_SR04::MP_HC_SR04(uint8_t echo, uint8_t trig, const String &tag)
-	:trig(trig)
-	,echo(echo)
-	,tag(tag)
+const char ok[] PROGMEM = "OK";
+const char* const errors_p[] PROGMEM = {ok};
+
+const char* const* MP_HY_SR04::ERRORS = errors_p;
+
+MP_HY_SR04::MP_HY_SR04(uint8_t echo, uint8_t trig)
+  : trig(trig), echo(echo)
 {
+
 }
 
-void MP_HC_SR04::init()
-{
+int MP_HY_SR04::init()  
+{	
+    this->cm = 0;
 	pinMode(this->trig, OUTPUT);
 	pinMode(this->echo, INPUT);
+    return 0;
 }
 
-double MP_HC_SR04::getDistance()
+void MP_HY_SR04::update(unsigned long current_time)
 {
-	long cm = 0;
-	/* loop for filter an hardware error */
-	do{
-		digitalWrite(trig, LOW);
-		delayMicroseconds(2);
-		digitalWrite(trig, HIGH);
-		delayMicroseconds(5);
-		digitalWrite(trig, LOW);
-		cm = pulseIn(echo, HIGH) / 29.0 / 2.0;
-	}while(cm > 3000);
-	return cm;
+    /* loop for filter an hardware error */
+    do {
+        digitalWrite(trig, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trig, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trig, LOW);
+        cm = pulseIn(echo, HIGH) / 29.0 / 2.0 ;
+  	} while(cm>3000);
+}
+
+void MP_HY_SR04::printStatus()
+{
+    Serial.print(F("distance (cm.) = "));
+    Serial.println(this->cm);
+}
+
+double MP_HY_SR04::getDistance() 
+{
+    return cm;
 }
--- lib/arduino/MP_HC_SR04/MP_HC_SR04.h
+++ lib/arduino/MP_HC_SR04/MP_HC_SR04.h
@@ -1,19 +1,23 @@
-#ifndef MP_HC_SR04_H
-#define MP_HC_SR04_H
+#ifndef MP_HY_SR04_H
+#define MP_HY_SR04_H
 
 #include <Arduino.h>
 
-class MP_HC_SR04
+class MP_HY_SR04
 {
 public:
-	MP_HC_SR04(uint8_t echo, uint8_t trig, const String &tag);
+    MP_HY_SR04(uint8_t echo ,uint8_t trig);
+	int init();
+	void update(unsigned long current_time);
+	void printStatus();
+	static const char* const* ERRORS;
 
-	void init();
-	double getDistance();
+    double getDistance();
 
 private:
-	uint8_t trig;
-	uint8_t echo;
-	const String tag;
-}
+    uint8_t trig;
+    uint8_t echo;
+    double cm;
+};
+
 #endif
