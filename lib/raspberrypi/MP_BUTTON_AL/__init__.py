ad343fc3a13eddf5a31b8c920188c29895992283
jetstreamc <jetstream.c@gmail.com>
2019-02-08 19:43:08 +0700

    - Add platform RASPBERRYPI to Generic Button and HC_SR04 - Add new device: RCW-0001 ultrasonic ranger

---
+++ lib/raspberrypi/MP_BUTTON_AL/__init__.py
@@ -0,0 +1,48 @@
+from MakerPlayground.Devices.MP_Device import MP_Device
+from enum import Enum
+import pigpio
+import time
+
+class State(Enum):
+    NOTHING = 1
+    PRESSED = 2
+    JUST_RELEASE = 3
+
+class MP_BUTTON_AL(MP_Device):
+
+    def __init__(self, pin):
+        self.pin = pin
+        self.pi = pigpio.pi()
+
+        self.pi.set_mode(pin, pigpio.INPUT)
+        self.pi.set_pull_up_down(pin, pigpio.PUD_OFF)
+ 
+        self.checkpoint = time.time()
+        self.state = State.NOTHING
+
+    def update(self, currentTime):
+        if self.state == State.NOTHING and self.isPress():
+            self.state = State.PRESSED
+        
+        if self.state == State.PRESSED and self.isNotPress():
+            self.state = State.JUST_RELEASE
+            checkpoint = currentTime
+        
+        if self.state == State.JUST_RELEASE and currentTime - checkpoint >= 0.150:
+            self.state = State.NOTHING
+
+    def getStatus(self):
+        return "Button is {0}".format("pressed" if self.isPress() else "not pressed")
+
+    def isPress(self):
+        return self.pi.read(self.pin) == 0
+
+    def isPressAndRelease(self):
+        if self.state == State.JUST_RELEASE:
+            self.state = State.NOTHING
+            self.checkpoint = time.time()
+            return True
+        return False
+
+    def isNotPress(self):
+        return self.pi.read(self.pin) == 1
---
+++ lib/raspberrypi/MP_HC_SR04/__init__.py
@@ -0,0 +1,51 @@
+from MakerPlayground.Devices.MP_Device import MP_Device
+import pigpio
+import time
+
+class MP_HC_SR04(MP_Device):
+    
+    timeout = 0.03
+
+    def __init__(self, trig, echo):
+        self.pi = pigpio.pi()
+        self.pi.set_mode(trig, pigpio.OUTPUT)
+        self.pi.set_mode(echo, pigpio.INPUT)
+
+        self.trig = trig
+        self.echo = echo
+        self.cm = 0
+        self.nextReading = 0;
+
+    def update(self, currentTime):
+        if currentTime >= self.nextReading:
+            self.nextReading = currentTime + 0.050
+            
+            # create pulse
+            self.pi.write(self.trig, pigpio.LOW)
+            time.sleep(0.000002)
+            self.pi.write(self.trig, pigpio.HIGH)
+            time.sleep(0.000010)
+            self.pi.write(self.trig, pigpio.LOW)
+            
+            startTime = stopTime = 0
+
+            # save startTime
+            while self.pi.read(self.echo) == pigpio.LOW:
+                startTime = time.time()
+
+            # save time of arrival
+            while self.pi.read(self.echo) == pigpio.HIGH and stopTime - startTime < MP_HC_SR04.timeout:
+                stopTime = time.time()
+
+            interval = stopTime - startTime
+            if interval <= MP_HC_SR04.timeout:
+                self.cm = (interval * 1000000) / 29 / 2
+
+            if self.cm > 450.0:
+                self.cm = 0
+
+    def getStatus(self):
+        return "distance (cm.) = {0}".format(self.cm)
+
+    def getDistance(self):
+        return self.cm
