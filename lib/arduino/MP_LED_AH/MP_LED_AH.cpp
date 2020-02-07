12c134afb3e7326bb81bc01211ceb4bb5cb2e821
Jui <46265316+Patthachaput@users.noreply.github.com>
2020-02-07 20:54:30 +0700

    Add Cytron Maker UNO

--- lib/arduino/MP_LED_AH/MP_LED_AH.cpp
+++ lib/arduino/MP_LED_AH/MP_LED_AH.cpp
@@ -26,12 +26,37 @@ void MP_LED_AH::printStatus()
 void MP_LED_AH::on(int brightness)
 {
 	this->brightness = brightness;
+	//if microcontroller is a AVR type will do in this part 
+#ifdef __AVR__
+	//check this pin has PWM or not
+	if(digitalPinHasPWM(this->pin))
+	{
+		analogWrite(this->pin, map(brightness, 0, 100, 0, 255));
+	}
+	else
+	{
+		digitalWrite(this -> pin, brightness > 0 ? HIGH : LOW);
+	}
+	//else will do this part
+#else
 	analogWrite(this->pin, map(brightness, 0, 100, 0, 255));
+#endif
 }
 
 void MP_LED_AH::off()
 {
 	this->brightness = 0;
 	// digitalWrite and analogWrite don't mix well in ESP32 so we use analogWrite with duty cycle = 0% instead
+#ifdef __AVR__
+	if(digitalPinHasPWM(this->pin))
+	{
+		analogWrite(this->pin, 0);
+	}
+	else
+	{
+		digitalWrite(this -> pin, LOW);
+	}
+#else
 	analogWrite(this->pin, 0);
+#endif
 }
