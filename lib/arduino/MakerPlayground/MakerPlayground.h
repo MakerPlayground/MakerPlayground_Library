c8d47705ee21629362848bb2da4642a39115d87e
Nuntipat Narkthong <nuntipat@gmail.com>
2020-04-30 21:04:44 +0700

    Implement animation logic for the C++ code generator

--- lib/arduino/MakerPlayground/MakerPlayground.h
+++ lib/arduino/MakerPlayground/MakerPlayground.h
@@ -19,50 +19,128 @@
     #define MP_ERR(name, status_code) PR_ERR(F(name)); Serial.println(reinterpret_cast<const __FlashStringHelper *>(ERRORS[status_code])); PR_END();
 #endif
 
-typedef void (*Task)(void);
-struct Expr {
-    double (*fn)(void) = NULL;
-    double value = 0.0;
-    unsigned int interval = 0;
-    unsigned long latestUpdateTime = 0;
-};
-
 uint8_t status_code = 0;
 unsigned long currentTime = 0;
 unsigned long latestLogTime = 0;
 
 void update();
 
-void evaluateExpression(Task task, Expr expr[], int numExpr) {
-    if (task != NULL) {
-        for (int i=0; i<numExpr; i++) {
-            if (expr[i].fn != NULL && currentTime - expr[i].latestUpdateTime >= expr[i].interval) {
-                expr[i].value = expr[i].fn();
-                expr[i].latestUpdateTime = currentTime;
-                task();
-            }
-        }
-    }
-}
-
-void setExpression(Expr& expr, double (*fn)(void), unsigned int interval) {
-    expr.fn = fn;
-    expr.value = fn();
-    expr.interval = interval;
-    expr.latestUpdateTime = currentTime;
-}
-
-void clearExpression(Expr& expr) {
-    expr.fn = NULL;
-}
-
-void setTask(Task& task, void (*fn)(void)) {
-    task = fn;
-    task();
-}
-
 double map(double x, double in_min, double in_max, double out_min, double out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+typedef void (*Task)(double);
+
+// easing function
+
+double Linear(double d) {
+    return d;
+}
+
+double EaseInExpo(double d) {
+    return d == 0 ? 0 : pow(2, 10 * d - 10);
+}
+
+template<int c1x, int c1y, int c2x, int c2y>
+double bezierCurve(double d) {
+    double p1x, p1y, p2x, p2y, p3x, p3y;
+    p1x = d * (c1x/100.0);
+    p1y = d * (c1y/100.0);
+    p2x = (1.0-d) * (c1x/100.0) + d * (c2x/100.0);
+    p2y = (1.0-d) * (c1y/100.0) + d * (c2y/100.0);
+    p3x = (1.0-d) * (c2x/100.0) + d;
+    p3y = (1.0-d) * (c2y/100.0) + d;
+
+    p1x = (1.0-d) * p1x + d * p2x;
+    p1y = (1.0-d) * p1y + d * p2y;
+    p2x = (1.0-d) * p2x + d * p3x;
+    p2y = (1.0-d) * p2y + d * p3y;
+
+    p1x = (1.0-d) * p1x + d * p2x;
+    p1y = (1.0-d) * p1y + d * p2y;
+    return p1y;
+}
+
+template <class T>
+class ValueAnimator {
+public:
+    T getValue(unsigned long currentTime) const {
+        return easing(constrain((double) (currentTime - startTime) / duration, 0.0, 1.0)) * (endValue - startValue) + startValue;
+    }
+
+    double (*easing)(double);
+    T startValue;
+    T endValue;
+    unsigned long duration;
+    unsigned long startTime;
+};
+
+template <int size>
+class NumericValueLookup{
+public:
+    double getValue(unsigned long currentTime) {
+        unsigned long period = currentTime - startTime;
+        int i;
+        for (i=count-1; i>0; i--) { // use > 0 so that i is clipped to 0 when period is invalid
+            if (time[i] < period) {
+                break;
+            }
+        }
+        return value[i];
+    }
+
+    double value[size];
+    unsigned long time[size];
+    int count;
+    unsigned long startTime;
+};
+
+const int BUFFER_SIZE 64;
+char buffer[BUFFER_SIZE];
+
+template <int size>
+class StringValueLookup{
+public:
+    char* getValue(char* buffer, unsigned long currentTime) const {
+        unsigned long period = currentTime - startTime;
+        int i;
+        for (i=count-1; i>0; i--) { // use > 0 so that i is clipped to 0 when period is invalid
+            if (time[i] < period) {
+                break;
+            }
+        }
+
+        const char* s = (char *) pgm_read_word(&(value[i]));
+        char* ptr = buffer;
+
+        char c;
+        while ((c = pgm_read_byte(s)) != '\0') {
+            if (c == '%') {
+                s++;
+                c = pgm_read_byte(s);
+                if (c == '%') {
+                    *ptr = c;
+                    ptr++;
+                } else {
+                    dtostrf(arg[c - '0'], 1, 2, ptr);
+                    while (*ptr != '\0')
+                        ptr++;
+                }
+            } else {
+                *ptr = c;
+                ptr++;
+            }
+            s++;
+        }
+        *ptr = '\0';
+        return buffer;
+    }
+
+    const char* const* value;
+    unsigned long time[size];
+    double arg[size];
+    int count;
+    unsigned long startTime;
+};
 
 #endif
