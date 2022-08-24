b59b0967f2d00a4f0f8dd1e425d7cfad07864a2b
Nuntipat Narkthong <nuntipat@gmail.com>
2022-08-24 10:47:15 +0700

    Remove ESP32 polyfill library as the function has been implemented upstream

--- lib/arduino/MP_DEVICE/MP_DEVICE.h
+++ lib/arduino/MP_DEVICE/MP_DEVICE.h
@@ -6,10 +6,6 @@
 #include "MP_ERROR.h"
 #include "MP_INTERACTIVE.h"
 
-#ifdef ESP32
-    #include "ESP32_Polyfill.h"
-#endif
-
 #if defined(ESP32) || defined(ESP8266)
     #include "MP_RECORD.h"
 #endif
